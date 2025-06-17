a789e1856a74ca652fb1dd96b9a76d6c446befaa
Shakir2010 <Shakirq@outlook.com>
2025-06-17 16:52:32 +0530

    added the config settings.

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -6,14 +6,17 @@ namespace zure_webapp_Learning.Pages;
 public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
+    private IConfiguration _Configuration;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public PrivacyModel(ILogger<PrivacyModel> logger,IConfiguration configuration)
     {
         _logger = logger;
+        _Configuration = configuration;
     }
 
     public void OnGet()
     {
+        ViewData["configs"] = _Configuration["Greeting"];
     }
 }
 
